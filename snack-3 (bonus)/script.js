8d4d97b9773d57e88cd2bd9848a6df3472f22fbc
ilariamotta <ilaria.motta93@gmail.com>
2025-11-10 17:01:46 +0100

    feat: impostazione logica snack3

--- snack-3 (bonus)/script.js
+++ snack-3 (bonus)/script.js
@@ -1,2 +1,9 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Non usare i metodi di array di JS 
+
+//1. creo un array di partenza
+//2. creo una funzione che scorre i numeri all'interno dell'array e identifica i numeri che hanno un indice compreso tra due numeri
+    //POI li inserisco in un nuovo array
+//3.evoco la funzione che stamperà l'array nuovo
+
+
