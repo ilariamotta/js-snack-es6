e7cb439b3b5fe530d43b20739f0e0c32d5c05034
ilariamotta <ilaria.motta93@gmail.com>
2025-11-10 15:24:28 +0100

    feat: sequenza logica snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,1 +1,22 @@
-console.log("Ciao");
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo oppure in console la bici con peso minore.
+
+// 1. creo array di oggetti
+// 2. creo un ciclo for dove scorro le bici e verifico SE quella attuale è più leggera della precedente, SALVO la bici che pesa meno
+// 3. stampo in pagina o console la bici con peso minore
+
+
+//1. ARRAY DI OGGETTI
+const biciDaCorsa = [
+  { nome: "Pinarello Dogma F", peso: 6.8 },
+  { nome: "Specialized Tarmac SL8", peso: 6.7 },
+  { nome: "Canyon Aeroad CF SLX", peso: 7.2 },
+  { nome: "Trek Émonda SLR", peso: 6.9 },
+  { nome: "Giant Propel Advanced", peso: 7.4 },
+  { nome: "Bianchi Oltre RC", peso: 7.1 },
+  { nome: "Cervélo R5", peso: 6.8 },
+  { nome: "Scott Addict RC", peso: 7.0 }
+];
+
+//2. CICLO FOR
