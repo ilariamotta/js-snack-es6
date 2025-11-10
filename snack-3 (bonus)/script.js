2d148d7d58b2fbdab21aba09f9de18d17ee591bf
ilariamotta <ilaria.motta93@gmail.com>
2025-11-10 17:17:44 +0100

    feat: snack3 completato

--- snack-3 (bonus)/script.js
+++ snack-3 (bonus)/script.js
@@ -6,4 +6,28 @@
     //POI li inserisco in un nuovo array
 //3.evoco la funzione che stamperà l'array nuovo
 
+//1. creo un array di partenza
 
+                // 0    1   2   3   4   5   6   7   8
+let arrayNumeri = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+//2. creo la funzione
+
+
+
+function nuovoArray (array, a, b) {
+    let arrayAggiornato = [];
+    for (let i = 0; i < array.length; i++)  {
+        let curNumero = array[i];
+        if (i >= a && i <= b) {
+            arrayAggiornato.push(curNumero);
+        }
+    }
+return arrayAggiornato;
+}
+
+
+//3. invoco la funzione
+
+let risultato = nuovoArray(arrayNumeri, 5, 6);
+console.log(risultato);
